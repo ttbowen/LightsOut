bed9c544bb8a48437fa6e3a7b8ef5ad7754d5caf
Thomas Bowen <6600748+ttbowen@users.noreply.github.com>
2021-04-09 02:10:53 +0100

    Create the game board and implement the basic game logic

---
+++ LightsOut/Cell.cs
@@ -0,0 +1,21 @@
+﻿namespace LightsOut
+{
+    public class Cell
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool IsLit { get; set; }
+
+        public Cell(int row, int column, bool isLit)
+        {
+            Row = row;
+            Column = column;
+            IsLit = isLit;
+        }
+
+        public void Flip()
+        {
+            IsLit = !IsLit;
+        }
+    }
+}
---
+++ LightsOut/Game.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace LightsOut
+{
+    public class Game
+    {
+        /// <summary>
+        /// The max number of rows on the board.
+        /// </summary>
+        public const int MaxRows = 5;
+
+        /// <summary>
+        /// The max number of columns on the board.
+        /// </summary>
+        public const int MaxColumns = 5;
+
+        /// <summary>
+        /// Gets or sets the game board cells.
+        /// </summary>
+        public Cell[,] Cells { get; set; }
+
+        /// <summary>
+        /// Gets whether the player has won.
+        /// </summary>
+        public bool HasWon
+        {
+            get => Cells.Cast<Cell>().All(cell => cell.IsLit);  
+        }
+
+        private Random random = new Random();
+
+        public Game()
+        {
+            Cells = new Cell[MaxRows, MaxColumns];
+        }
+
+        /// <summary>
+        /// Flip the cell at the given row and column.
+        /// </summary>
+        /// <param name="row">The row of the cell.</param>
+        /// <param name="column">The column of the cell.</param>
+        public void Flip(int row, int column)
+        {
+            if (row + 1 < MaxRows)
+            {
+                Cells[row + 1, column].Flip();
+            }
+
+            if (row - 1 >= 0)
+            {
+                Cells[row - 1, column].Flip();
+            }
+
+            if (column + 1 < MaxColumns)
+            {
+                Cells[row, column + 1].Flip();
+            }
+
+            if (column - 1 >= 0)
+            {
+                Cells[row, column - 1].Flip();
+            }
+        }
+
+        /// <summary>
+        /// Creates the game board state.
+        /// </summary>
+        public void CreateBoard()
+        {
+            for (int row = 0; row < MaxRows; row++ )
+            {
+                for (int column = 0; column < MaxColumns; column++)
+                {
+                    var randomNumber = random.Next(0, 10);
+                    Cells[row, column] = new Cell(row, column, randomNumber > 3);
+                }
+            }
+        }
+    }
+}
